7bbee08be121240b054dcbdbfd9e1a60b13f4642
heleneliasi <hedaa012@osloskolen.no>
2026-05-31 22:57:36 +0200

    css

--- app.py
+++ app.py
@@ -5,12 +5,68 @@ from waitress import serve
 import os
 from dotenv import load_dotenv, find_dotenv
 
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)  #last .env fila
+
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY", "backup_nøkkel")
 
 @app.route('/')
-def forside():
+def index():
      return render_template('index.html')
+
+@app.route("/services", methods=["GET", "POST"])
+def services_side():
+     mydb = get_connection()
+     cursor = mydb.cursor()
+     cursor.execute("SELECT * FROM service")
+     services = cursor.fetchall()
+     mydb.close()
+     return render_template("service.html", services=services)
+
+@app.route("/book", methods=["GET", "POST"])
+def login_registrer():
+     if "user_id" not in session:
+          flash("Du må logge inn for å bestille time")
+          return redirect("/login")
+     
+     mydb = get_connection()
+     cursor = mydb.cursor()
+
+     if request.method == "POST":
+          service_id = request.form["service"]
+          date = request.form["dato"]
+          time = request.form["tid"]
+
+          cursor.execute(
+               "INSERT INTO appointment (user_id, service_id, date, time) VALUES (%s,%s,%s,%s)",
+               (session["user_id"], service_id, date, time)
+          )
+          mydb.commit()
+
+          cursor.execute("SELECT name FROM service WHERE id=%s", (service_id,))
+          service = cursor.fetchone()
+          mydb.close()
+
+          return render_template("confirmation.html",
+                navn=session["username"],
+                tjeneste=service[0],
+                dato=date,
+                tid=time
+            )
+     
+     cursor.execute("SELECT * FROM service")
+     services = cursor.fetchall()
+     mydb.close()
+
+     return render_template("book.html", services=services)
+
+
+
+
+
+
+
 
 
 
